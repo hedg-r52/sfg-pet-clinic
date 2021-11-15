d05ad6eb4a0796970e233109c061a0b1b350bf54
Andrei Soloviev <hedg.r52@gmail.com>
2021-11-15 21:01:39 +0300

    added visits, update owner with pet relationship. closes #25

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,8 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Andrei Soloviev (hedg.r52@gmail.com).
  * date: 04.11.2021
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
