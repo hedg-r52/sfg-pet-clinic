63c7d11292e73ad05d319414c6c52c27c797e26e
Andrei Soloviev <hedg.r52@gmail.com>
2021-11-15 22:15:41 +0300

    added PetTypeService. closes #28

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by Andrei Soloviev (hedg.r52@gmail.com).
+ * date: 15.11.2021
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
