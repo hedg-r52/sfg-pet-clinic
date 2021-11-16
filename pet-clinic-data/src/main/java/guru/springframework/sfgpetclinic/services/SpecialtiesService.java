7d469fb4f64e8ba36b5d5214841102ea3c70a9f3
Andrei Soloviev <hedg.r52@gmail.com>
2021-11-16 07:57:31 +0300

    added Specialties Map Impl. closes #30

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by Andrei Soloviev (hedg.r52@gmail.com).
+ * date: 16.11.2021
+ */
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
