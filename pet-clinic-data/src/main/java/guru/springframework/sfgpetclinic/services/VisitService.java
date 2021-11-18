5fee964564078d4987ae5caae642497d3a5de275
Andrei Soloviev <hedg.r52@gmail.com>
2021-11-18 14:20:47 +0300

    Implemented Map impl for Visit Service, closes #45

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * Created by Andrei Soloviev (hedg.r52@gmail.com).
+ * date: 18.11.2021
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,45 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.VisitService;
+
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Created by Andrei Soloviev (hedg.r52@gmail.com).
+ * date: 18.11.2021
+ */
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (Objects.isNull(visit.getPet())
+                || Objects.isNull(visit.getPet().getId())
+                || Objects.isNull(visit.getPet().getOwner())
+                || Objects.isNull(visit.getPet().getOwner().getId())
+        ) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
